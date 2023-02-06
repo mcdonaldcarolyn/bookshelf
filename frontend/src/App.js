321ab7d4e5a7a6f36b6b5310f0d7e2c3b7042d8e
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2023-02-06 09:25:09 -0500

    add add to index and update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { AddBook } from "./add-book/compponents";
+import { BookShelf } from "./bookshelf/components";
+import { useEffect, getFinishedList, getUnfinishedList, saveBook, setFinished } from "./repository";
+import { Col, Container, Navbar, Row } from "react-bootstrap";
 
 function App() {
+  const [unfinishedList, setUnfinishedList] = useState([]);
+  const [finishedList, setFinishedList] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let unfinishedList = await getUnfinishedList();
+      setUnfinishedList(unfinishedList);
+
+      let finishedList = await getFinishedList();
+      setFinishedList(finishedList);
+    }
+    fetchData();
+  }, []);
+
+  const handleAddBook = async (book) => {
+    let savedBook = await saveBook(book);
+    if (savedBook.finished) {
+      setFinishedList([...finishedList, savedBook]);
+    } else {
+      setUnfinishedList([...unfinishedList, savedBook]);
+    }
+  }
+
+  const handleToggleBook = async (book) => {
+    const moveBook = (sourceList, setSourceList, destList, setDestList) => {
+      console.log(book);
+      const newSourceList = sourceList.filter((mBook) => {
+        return mBook.id !== book.id;
+      });
+      setSourceList(newSourceList);
+
+      const newDestList = [...destList, { ...book, finished: !book.finished }];
+      setDestList(newDestList);
+    };
+
+    await setFinished(book, !book.finished);
+
+    if (book.finished) {
+      moveBook(
+        finishedList,
+        setFinishedList,
+        unfinishedList,
+        setUnfinishedList
+      );
+    } else {
+      moveBook(
+        unfinishedList,
+        setUnfinishedList,
+        finishedList,
+        setFinishedList
+      );
+    }
+  };
+
+  const handleDeleteBook = (book) => {
+    console.log(book.id);
+    const deleteFromList = (list, setList) => {
+      const newList = list.filter((mBook) => mBook.id !== book.id);
+      setList(newList);
+    };
+
+    deleteBook(book).then((_) => {
+      if (book.finished) {
+        deleteFromList(finishedList, setFinishedList);
+      } else {
+        deleteFromList(unfinishedList, setUnfinishedList);
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Navbar bg="light" className="mx-2">
+          <Navbar.Brand className="navbar-brand" href="#">
+            Bookshelf
+          </Navbar.Brand>
+        </Navbar>
       </header>
+      <Container className="mt-3">
+        <article className="row">
+          <Row>
+            <Col lg={4}>
+              <AddBook handleAddBook={handleAddBook} />
+            </Col>
+            <Col lg={4}>
+              <Bookshelf
+                finished={false}
+                bookList={unfinishedList}
+                handleToggleBook={handleToggleBook}
+                handleDeleteBook={handleDeleteBook}
+              />
+            </Col>
+            <Col lg={4}>
+              <Bookshelf
+                finished={true}
+                bookList={finishedList}
+                handleToggleBook={handleToggleBook}
+                handleDeleteBook={handleDeleteBook}
+              />
+            </Col>
+          </Row>
+        </article>
+      </Container>
     </div>
   );
 }
